c114861752bcd920822a5e0c376e511b4ccc9b16
bannerOfFaith <564128352@qq.com>
2016-06-14 19:31:48 +0800

    上传初始工程

---
+++ 网易新闻/网易新闻/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  网易新闻
+//
+//  Created by 李洋 on 16/6/14.
+//  Copyright © 2016年 李洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 网易新闻/网易新闻/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  网易新闻
+//
+//  Created by 李洋 on 16/6/14.
+//  Copyright © 2016年 李洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
